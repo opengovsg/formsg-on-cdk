ebf3fe37c44bab7990f27f8c16da7fb86c8de617
LoneRifle <LoneRifle@users.noreply.github.com>
2024-07-29 17:48:31 +0800

    feat: stub a Fargate-based ECS service

--- lib/formsg-on-cdk-stack.ts
+++ lib/formsg-on-cdk-stack.ts
@@ -1,16 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
 
 export class FormsgOnCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // ECS cluster
+    const vpc = new ec2.Vpc(this, 'vpc', { maxAzs: 2 });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FormsgOnCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cluster = new ecs.Cluster(this, 'ecs', { vpc });
+
+    // Create Fargate Service
+    const fargate = new ecsPatterns.NetworkLoadBalancedFargateService(this, 'app', {
+      cluster,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample')
+      },
+    });
+
+    const scaling = fargate.service.autoScaleTaskCount({ maxCapacity: 2 });
+    scaling.scaleOnCpuUtilization('scaling', {
+      targetUtilizationPercent: 50,
+      scaleInCooldown: cdk.Duration.seconds(60),
+      scaleOutCooldown: cdk.Duration.seconds(60)
+    });
+
+    new cdk.CfnOutput(this, 'LoadBalancerDNS', { value: fargate.loadBalancer.loadBalancerDnsName });
   }
 }
