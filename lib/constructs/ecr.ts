ee59eb962ac6bb3d3ffd214eb484c5640f5115a8
LoneRifle <LoneRifle@users.noreply.github.com>
2024-08-13 20:56:35 +0800

    feat: add ECR repository for Form images

--- lib/constructs/ecr.ts
+++ lib/constructs/ecr.ts
@@ -2,6 +2,7 @@ import { Construct } from 'constructs'
 import * as ecrdeploy from 'cdk-ecr-deployment'
 import { Repository } from 'aws-cdk-lib/aws-ecr'
 import * as ecr from 'aws-cdk-lib/aws-ecr-assets'
+import { RemovalPolicy } from 'aws-cdk-lib'
 
 export interface FormsgEcrProps {
 }
@@ -10,21 +11,34 @@ export class FormsgEcr extends Construct {
   readonly lambdaVirusScanner: {
     repository: Repository
   }
+  readonly form: {
+    repository: Repository
+  }
 
   constructor(
     scope: Construct, 
     props: FormsgEcrProps = {}
   ) {
     super(scope, 'ecr')
-    const repository = new Repository(this, 'lambda-virus-scanner', {
+    const repositoryLambdaVirusScanner = new Repository(this, 'lambda-virus-scanner', {
       repositoryName: 'lambda-virus-scanner',
+      removalPolicy: RemovalPolicy.DESTROY,
     })
     // new ecrdeploy.ECRDeployment(scope, 'ecr-deployment-lambda-virus-scanner', {
     //   src: new ecrdeploy.DockerImageName('opengovsg/lambda-virus-scanner:latest'),
     //   dest: new ecrdeploy.DockerImageName(this.lambdaVirusScanner.repositoryUriForTag('latest')),
     // })
     this.lambdaVirusScanner = {
-      repository,
+      repository: repositoryLambdaVirusScanner,
+    }
+
+
+    const repositoryForm = new Repository(this, 'form', {
+      repositoryName: 'form',
+      removalPolicy: RemovalPolicy.DESTROY,
+    })
+    this.form = {
+      repository: repositoryForm,
     }
   }
 
