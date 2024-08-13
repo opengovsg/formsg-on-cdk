4bc07f1b7db1c57b66c6b44310c09b6478c66478
LoneRifle <LoneRifle@users.noreply.github.com>
2024-08-13 11:33:25 +0800

    fix: avoid cdk bootstrap, not using cdk

--- bin/formsg-on-cdk.ts
+++ bin/formsg-on-cdk.ts
@@ -8,6 +8,9 @@ const app = new cdk.App()
 const withHttps = process.env.WITH_HTTPS === 'true'
 
 new FormsgOnCdkStack(app, 'form', withHttps, {
+  synthesizer: new cdk.DefaultStackSynthesizer({
+    generateBootstrapVersionRule: false
+  })
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
